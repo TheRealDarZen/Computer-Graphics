7e932617f738e145453c38cdc4309ffb099ad6b0
darzen <nivnik004@gmail.com>
2025-03-09 14:03:55 +0100

    Completed Task 3 from List 1.

---
+++ List_1/src/Task_3.java
@@ -0,0 +1,25 @@
+import java.awt.image.*;
+import java.awt.Color;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        ImageSaver imageSaver = new ImageSaver();
+
+        int width = 1000, height = 1000;
+        int squareSize = args.length > 0 ? Integer.parseInt(args[0]) : 100;
+        Color color1 = args.length > 1 ? Color.decode(args[1]) : Color.WHITE;
+        Color color2 = args.length > 2 ? Color.decode(args[2]) : Color.BLACK;
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                boolean isColor1 = ((x / squareSize) % 2 == (y / squareSize) % 2);
+                Color color = isColor1 ? color1 : color2;
+                image.setRGB(x, y, color.getRGB());
+            }
+        }
+
+        imageSaver.saveImage(image, "task_3");
+    }
+}
